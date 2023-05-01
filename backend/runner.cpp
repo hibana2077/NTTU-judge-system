4a59578b4ad61309f4f8136acc7d46538e15f564
hibana2077 <hibana2077@gmail.com>
2023-05-01 20:36:44 +0800

    add runner using envcheck

--- backend/runner.cpp
+++ backend/runner.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: hibana2077 hibana2077@gmail.com
+ * @Date: 2023-04-27 17:57:53
+ * @LastEditors: hibana2077 hibana2077@gmail.com
+ * @LastEditTime: 2023-05-01 20:36:43
+ * @FilePath: \NTTU-new-gen-judge-system\backend\runner.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 
 #include <iostream>
@@ -17,6 +25,10 @@ ins_mp compile = {
 format:
 ./runner lang=cpp sourcefile=main.cpp inputfile=input.txt outputfile=output.txt
 */
+
+ins_mp util_map = {
+    {"envcheck","./envcheck.sh"},
+};
 
 ins_mp run = {
     {"cpp","./out "},
