fdc0bbca3d07f0857728cef011a5f1ee0a8725fa
hibana2077 <hibana2077@gmaill.com>
2023-05-01 17:37:52 +0800

    tring nuxt js framework

--- nttu-judge/app.vue
+++ nttu-judge/app.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Author: hibana2077 hibana2077@gmaill.com
+ * @Date: 2023-04-29 03:54:10
+ * @LastEditors: hibana2077 hibana2077@gmaill.com
+ * @LastEditTime: 2023-05-01 17:31:50
+ * @FilePath: /NTTU-new-gen-judge-system/nttu-judge/src/app.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
---
+++ nttu-judge/components/IronManWelcome.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Author: hibana2077 hibana2077@gmaill.com
+ * @Date: 2023-05-01 17:16:21
+ * @LastEditors: hibana2077 hibana2077@gmaill.com
+ * @LastEditTime: 2023-05-01 17:17:39
+ * @FilePath: /NTTU-new-gen-judge-system/nttu-judge/components/IronManWelcome.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <div class="bg-white py-24">
+      <div class="flex flex-col items-center">
+        <h1 class="text-6xl font-semibold text-sky-400">2023 iThome</h1>
+        <p class="mt-4 text-9xl font-bold text-gray-600">鐵人賽</p>
+      </div>
+    </div>
+  </template>
