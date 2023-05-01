9aa0877e4f90aebf85e2e573dee803bf7a6d4dcb
hibana2077 <hibana2077@gmail.com>
2023-05-01 20:32:41 +0800

    add c env check

--- backend/runner.cpp
+++ backend/runner.cpp
@@ -1,11 +1,4 @@
-/*
- * @Author: hibana2077 hibana2077@gmail.com
- * @Date: 2023-04-27 17:57:53
- * @LastEditors: hibana2077 hibana2077@gmail.com
- * @LastEditTime: 2023-04-28 14:45:42
- * @FilePath: \NTTU-new-gen-judge-system\backend\runer.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+
 
 #include <iostream>
 #include <map>
