ebc6aea0ef2e52bc5abcb5d216f125b631942b99
hibana2077 <hibana2077@gmail.com>
2023-05-02 21:37:02 +0800

    update runner.cpp

--- backend/runner.cpp
+++ backend/runner.cpp
@@ -1,13 +1,3 @@
-/*
- * @Author: hibana2077 hibana2077@gmail.com
- * @Date: 2023-04-27 17:57:53
- * @LastEditors: hibana2077 hibana2077@gmail.com
- * @LastEditTime: 2023-05-01 20:36:43
- * @FilePath: \NTTU-new-gen-judge-system\backend\runner.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-
-
 #include <iostream>
 #include <map>
 #include <string>
