76e03ee59784f9ebdfc33ad39f46bb0012f3487e
hibana2077 <hibana2077@gmaill.com>
2023-05-02 16:15:44 +0800

    header half

--- nttu-judge/app.vue
+++ nttu-judge/app.vue
@@ -2,7 +2,7 @@
  * @Author: hibana2077 hibana2077@gmaill.com
  * @Date: 2023-04-29 03:54:10
  * @LastEditors: hibana2077 hibana2077@gmaill.com
- * @LastEditTime: 2023-05-02 15:50:14
+ * @LastEditTime: 2023-05-02 16:08:19
  * @FilePath: /NTTU-new-gen-judge-system/nttu-judge/src/app.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -14,3 +14,9 @@
     <NuxtLayout name="footer"></NuxtLayout>
   </div>
 </template>
+
+<style>
+body {
+  background-color: #eee;
+}
+</style>
--- nttu-judge/layouts/header.vue
+++ nttu-judge/layouts/header.vue
@@ -2,14 +2,21 @@
  * @Author: hibana2077 hibana2077@gmaill.com
  * @Date: 2023-05-02 14:44:41
  * @LastEditors: hibana2077 hibana2077@gmaill.com
- * @LastEditTime: 2023-05-02 14:50:30
+ * @LastEditTime: 2023-05-02 16:14:50
  * @FilePath: /NTTU-new-gen-judge-system/nttu-judge/layouts/Header.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <div>
+    <div style="background-color: white;">
       <el-container>
-        <el-header class="text-6xl font-semibold text-rose-400" style="background-color: #409EFF;">Header</el-header>
+        <el-header>
+            <el-text class="flex justify-center text-4xl font-bold text-gray-700">
+                NTTU OJ
+            </el-text>
+            <div style="display: flex; text-align: center;">
+                <el-avatar>H</el-avatar>
+            </div>
+        </el-header>
       </el-container>
       <slot />
     </div>
