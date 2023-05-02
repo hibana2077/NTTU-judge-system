60b9190a4a2e974a89a840fcdf5a4c9729db6b66
hibana2077 <hibana2077@gmaill.com>
2023-05-02 13:04:42 +0800

    tring nuxt

--- nttu-judge/pages/about.vue
+++ nttu-judge/pages/about.vue
@@ -2,43 +2,15 @@
  * @Author: hibana2077 hibana2077@gmaill.com
  * @Date: 2023-05-01 17:34:22
  * @LastEditors: hibana2077 hibana2077@gmaill.com
- * @LastEditTime: 2023-05-02 12:14:22
+ * @LastEditTime: 2023-05-02 13:04:31
  * @FilePath: /NTTU-new-gen-judge-system/nttu-judge/pages/about.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  <div class="p-4">
-    <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
-      <img
-        class="w-full"
-        src="https://tailwindcss.com/img/card-top.jpg"
-        alt="Sunset in the mountains"
-      />
-      <div class="px-6 py-4 pb-2">
-        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
-        </p>
-      </div>
-      <div class="px-6 py-4">
-        <span class="badge mr-2">#photography</span>
-        <span class="badge mr-2">#travel</span>
-        <span class="badge">#winter</span>
-      </div>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-yellow-400">大家好！我是 hibana2077</h1>
+      <p class="my-8 text-3xl text-gray-600">这里是 /about</p>
     </div>
-    <NuxtLink to="/about">
-      <TButton>About</TButton>
-    </NuxtLink>
   </div>
 </template>
-
-<style lang="postcss" scoped>
-.badge {
-  @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
-  &:hover {
-    @apply bg-gray-300;
-  }
-}
-</style>
