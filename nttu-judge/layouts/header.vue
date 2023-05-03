aaa6fe8bdd2e98760518f643cdf7a0afa1bd0bf0
hibana2077 <hibana2077@gmaill.com>
2023-05-03 12:49:02 +0800

    clean up

--- nttu-judge/layouts/header.vue
+++ nttu-judge/layouts/header.vue
@@ -2,7 +2,7 @@
  * @Author: hibana2077 hibana2077@gmail.com
  * @Date: 2023-05-02 21:36:33
  * @LastEditors: hibana2077 hibana2077@gmaill.com
- * @LastEditTime: 2023-05-03 11:52:46
+ * @LastEditTime: 2023-05-03 12:43:25
  * @FilePath: \NTTU-new-gen-judge-system\nttu-judge\layouts\header.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -13,6 +13,7 @@
     mode="horizontal"
     :ellipsis="false"
     @select="handleSelect"
+    menu-trigger="hover"
   >
     <el-menu-item index="0" class="font-semibold">
       <NuxtLink to="/">NTTUOJ</NuxtLink>
