8df50ec721c39aa6bbf353f248272a2f551d0990
hibana2077 <hibana2077@gmaill.com>
2023-05-09 14:15:36 +0800

    billboard

--- nttu-judge/pages/index.vue
+++ nttu-judge/pages/index.vue
@@ -2,7 +2,7 @@
  * @Author: hibana2077 hibana2077@gmaill.com
  * @Date: 2023-05-03 11:45:56
  * @LastEditors: hibana2077 hibana2077@gmaill.com
- * @LastEditTime: 2023-05-08 15:15:42
+ * @LastEditTime: 2023-05-08 16:24:33
  * @FilePath: /NTTU-new-gen-judge-system/nttu-judge/pages/index.vue 
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -11,7 +11,7 @@
     <template #header>
       <div class="card-header">
         <span class="font-bold">Billboard</span>
-        <el-button class="button" type="primary" round text>Refresh</el-button>
+        <el-button class="button" type="primary" round plain>Refresh</el-button>
       </div>
     </template>
     <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
