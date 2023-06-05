530c480d8dc0b36e5d69ad511d01c1c3c3505abd
hibana2077 <hibana2077@gmaill.com>
2023-06-05 10:28:24 +0800

    succ 2

---
+++ lab/script/testts.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: hibana2077 hibana2077@gmaill.com
+ * @Date: 2023-06-05 10:20:06
+ * @LastEditors: hibana2077 hibana2077@gmaill.com
+ * @LastEditTime: 2023-06-05 10:20:15
+ * @FilePath: /NTTU-new-gen-judge-system/lab/script/testts.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log("Hello World");
