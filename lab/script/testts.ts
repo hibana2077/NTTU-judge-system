04f247aa5e4078679885fe158bb0597d25e250d5
hibana2077 <hibana2077@gmaill.com>
2023-06-05 10:33:56 +0800

    typescript fail

--- lab/script/testts.ts
+++ lab/script/testts.ts
@@ -1,9 +1,25 @@
-/*
- * @Author: hibana2077 hibana2077@gmaill.com
- * @Date: 2023-06-05 10:20:06
- * @LastEditors: hibana2077 hibana2077@gmaill.com
- * @LastEditTime: 2023-06-05 10:20:15
- * @FilePath: /NTTU-new-gen-judge-system/lab/script/testts.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-console.log("Hello World");
+import * as readline from 'readline';
+
+function processInputData(callback: (input: string) => void) {
+  // Create readline interface
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  });
+
+  // Listen for lines of input
+  rl.on('line', (line) => {
+    callback(line);
+  });
+
+  rl.on('close', () => {
+    console.log('Input processing completed');
+    process.exit(0);
+  });
+}
+
+// Use the function
+processInputData((input) => {
+  console.log('Received input:', input);
+});
