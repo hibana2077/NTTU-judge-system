f35670c9f9fe44e3453dd9a2c2921f34f996c933
hibana2077 <hibana2077@gmaill.com>
2023-06-17 14:53:15 +0800

    update

---
+++ backend/sh_libary/python_judge.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+random=$RANDOM
+time_output="time_output_$random.txt"
+temp_output="temp_$random.txt"
+diff_output="diff_$random.txt"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        --code )           shift
+                            code=$1
+                            ;;
+        --in )             shift
+                            input=$1
+                            ;;
+        --ans )            shift
+                            ans=$1
+                            ;;
+        --out )            shift
+                            output=$1
+                            ;;
+        --mode )           shift
+                            mode=$1
+                            ;;
+        --uid )            shift
+                            uid=$1
+                            ;;
+        * )                 echo "Unknown flag"
+                            exit 1
+    esac
+    shift
+done
+
+/usr/bin/time -v python3 $code < $input > temp.txt 2> time_output.txt
+
+if [ "$mode" = "1" ]; then
+    diff -B -w "temp.txt" $ans > "diff.txt"
+else
+    diff -i "temp.txt" $ans > "diff.txt"
+fi
