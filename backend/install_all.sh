8f38706e4183f13ee96b52a691345f09023c73ac
hibana2077 <hibana2077@gmaill.com>
2023-06-17 11:29:02 +0800

    update

--- backend/install_all.sh
+++ backend/install_all.sh
@@ -1,10 +1,17 @@
+echo "regulate apt update"
+apt-get update
+echo "install vim curl git"
+apt-get install -y vim curl git
+echo "install c compiler"
+apt-get install -y gcc
 echo "install c++ compiler"
-apt-get install g++
+apt-get install -y g++
 echo "install nodejs"
-apt-get install nodejs
+apt-get install -y nodejs
 echo "install python3"
-apt-get install python3
-apt-get install python3-pip
+apt-get install -y python3
+apt-get install -y python3-pip
 echo "install rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
 echo "install done!"
