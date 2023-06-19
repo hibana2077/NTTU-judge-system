5624dd929e05411f1cd94b40f51b5bdfc36a455f
hibana2077 <hibana2077@gmaill.com>
2023-06-19 14:27:12 +0800

    add ytlink

--- backend/install_all.sh
+++ backend/install_all.sh
@@ -1,7 +1,8 @@
+
 echo "regulate apt update"
 apt-get update
-echo "install vim curl git"
-apt-get install -y vim curl git
+echo "install vim curl git wget"
+apt-get install -y vim curl git wget
 echo "install c compiler"
 apt-get install -y gcc
 echo "install c++ compiler"
@@ -11,4 +12,6 @@ apt-get install -y nodejs
 echo "install python3"
 apt-get install -y python3
 apt-get install -y python3-pip
+echo "install dart"
+#https://youtu.be/RFF4_n7Xpzg
 echo "install done!"
