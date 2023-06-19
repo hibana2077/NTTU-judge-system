d6264a974f6d49971f43e4734616064fdffe2516
hibana2077 <hibana2077@gmaill.com>
2023-06-19 13:51:18 +0800

    remove chinese

--- backend/envcheck.sh
+++ backend/envcheck.sh
@@ -1,48 +1,55 @@
 #!/bin/bash
-
+###
+ # @Author: hibana2077 hibana2077@gmaill.com
+ # @Date: 2023-06-17 11:17:06
+ # @LastEditors: hibana2077 hibana2077@gmaill.com
+ # @LastEditTime: 2023-06-19 13:50:50
+ # @FilePath: /NTTU-new-gen-judge-system/backend/envcheck.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
 output_file="envcheck.txt"
 
-echo "檢查C編譯器(gcc)..."
+echo "Checking C compiler (gcc)..."
 if command -v gcc >/dev/null 2>&1; then
-    echo "C編譯器(gcc)已安裝。"
+    echo "C compiler (gcc) is installed."
     gcc --version | head -n 1
-    "gcc : $(gcc --version | head -n 1)" >> $output_file
+    echo "gcc : $(gcc --version | head -n 1)" >> $output_file
 else
-    echo "C編譯器(gcc)未安裝。"
+    echo "C compiler (gcc) is not installed."
 fi
 
-echo "檢查C++編譯器(g++)..."
+echo "Checking C++ compiler (g++)..."
 if command -v g++ >/dev/null 2>&1; then
-    echo "C++編譯器(g++)已安裝。"
+    echo "C++ compiler (g++) is installed."
     g++ --version | head -n 1
-    "g++ : $(g++ --version | head -n 1)" >> $output_file
+    echo "g++ : $(g++ --version | head -n 1)" >> $output_file
 else
-    echo "C++編譯器(g++)未安裝。"
+    echo "C++ compiler (g++) is not installed."
 fi
 
-echo "檢查Node.js..."
+echo "Checking Node.js..."
 if command -v node >/dev/null 2>&1; then
-    echo "Node.js已安裝。"
+    echo "Node.js is installed."
     node --version
-    "node : $(node --version)" >> $output_file
+    echo "node : $(node --version)" >> $output_file
 else
-    echo "Node.js未安裝。"
+    echo "Node.js is not installed."
 fi
 
-echo "檢查Python 3..."
+echo "Checking Python 3..."
 if command -v python3 >/dev/null 2>&1; then
-    echo "Python 3已安裝。"
+    echo "Python 3 is installed."
     python3 --version
-    "python3 : $(python3 --version)" >> $output_file
+    echo "python3 : $(python3 --version)" >> $output_file
 else
-    echo "Python 3未安裝。"
+    echo "Python 3 is not installed."
 fi
 
-echo "檢查Rust..."
+echo "Checking Rust..."
 if command -v rustc >/dev/null 2>&1; then
-    echo "Rust已安裝。"
+    echo "Rust is installed."
     rustc --version
-    "rustc : $(rustc --version)" >> $output_file
+    echo "rustc : $(rustc --version)" >> $output_file
 else
-    echo "Rust未安裝。"
+    echo "Rust is not installed."
 fi
