7f90ca9c5c6fbc486a90842834ffc09a41a96579
hibana2077 <hibana2077@gmaill.com>
2023-06-19 14:00:09 +0800

    fix chinese bug

--- backend/envcheck.sh
+++ backend/envcheck.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-###
- # @Author: hibana2077 hibana2077@gmaill.com
- # @Date: 2023-06-17 11:17:06
- # @LastEditors: hibana2077 hibana2077@gmaill.com
- # @LastEditTime: 2023-06-19 13:50:50
- # @FilePath: /NTTU-new-gen-judge-system/backend/envcheck.sh
- # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-### 
 output_file="envcheck.txt"
 
 echo "Checking C compiler (gcc)..."
