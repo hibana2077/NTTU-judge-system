9f392e978ffb917d65fa65d80a73353a8b66c35e
hibana2077 <hibana2077@gmaill.com>
2023-08-20 20:15:29 +1000

    update

--- backend/sh_libary/python_judge.sh
+++ backend/sh_libary/python_judge.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-###
- # @Author: hibana2077 hibana2077@gmaill.com
- # @Date: 2023-06-17 16:27:30
- # @LastEditors: hibana2077 hibana2077@gmaill.com
- # @LastEditTime: 2023-08-20 20:15:00
- # @FilePath: /NTTU-new-gen-judge-system/backend/sh_libary/python_judge.sh
- # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-### 
-
 random=$RANDOM
 time_output="time_output_$random.txt"
 temp_output="temp_$random.txt"
