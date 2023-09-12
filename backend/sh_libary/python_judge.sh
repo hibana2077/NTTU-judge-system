f0ec3ba6082e0f00bc38aa10d892d08d3e61491a
hibana2077 <hibana2077@gmaill.com>
2023-09-12 13:32:39 +0800

    finish shell script

--- backend/sh_libary/python_judge.sh
+++ backend/sh_libary/python_judge.sh
@@ -1,33 +1,58 @@
-random=$RANDOM
+#!/bin/bash
+
+# Description: This script is used to judge python code
+# Input: --code <code_path> --in <input_path> --ans <ans_path> --out <output_path> --mode <mode> --uid <uid>
+
+random=$RANDOM #random number for temp file
 time_output="time_output_$random.txt"
 temp_output="temp_$random.txt"
 diff_output="diff_$random.txt"
+
+# Initializing flags to check if necessary parameters are provided
+code_flag=0
+in_flag=0
+ans_flag=0
+out_flag=0
+mode_flag=0
+uid_flag=0
 
 while [ "$1" != "" ]; do
     case $1 in
         --code )           shift
                             code=$1
+                            code_flag=1
                             ;;
         --in )             shift
                             input=$1
+                            in_flag=1
                             ;;
         --ans )            shift
                             ans=$1
+                            ans_flag=1
                             ;;
         --out )            shift
                             output=$1
+                            out_flag=1
                             ;;
         --mode )           shift
                             mode=$1
+                            mode_flag=1
                             ;;
         --uid )            shift
                             uid=$1
+                            uid_flag=1
                             ;;
-        * )                 echo "Unknown flag"
+        * )                 echo "未知的標誌"
                             exit 1
     esac
     shift
 done
+
+# Checking if all necessary parameters are provided
+if [ $code_flag -eq 0 ] || [ $in_flag -eq 0 ] || [ $ans_flag -eq 0 ] || [ $out_flag -eq 0 ] || [ $mode_flag -eq 0 ] || [ $uid_flag -eq 0 ]; then
+    echo "缺少必要的參數"
+    exit 1
+fi
 
 /usr/bin/time -v python3 $code < $input > $temp_output 2> $time_output
 
@@ -62,11 +87,11 @@ done < "$time_output"
 #read diff output
 echo "  diff_info:" >> $output
 
-while IFS= read -r line
-do
-    if [[ $line == *"differ"* ]]; then
-        echo "    diff: 1" >> $output
-    else
-        echo "    diff: 0" >> $output
-    fi
-done < "$diff_output"
+if grep -q 'differ' "$diff_output"; then
+    echo "    diff: 1" >> $output
+else
+    echo "    diff: 0" >> $output
+fi
+
+# Clean up the temporary files
+rm $time_output $temp_output $diff_output
