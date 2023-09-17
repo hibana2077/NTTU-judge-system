02970ae15aab488bdbd3f823f72664db953cec44
hibana2077 <hibana2077@gmail.com>
2023-09-17 14:04:08 +0800

    clean up

--- backend/apiServer.py
+++ backend/apiServer.py
@@ -1,11 +1,4 @@
-'''
-Author: hibana2077 hibana2077@gmaill.com
-Date: 2023-01-16 22:13:39
-LastEditors: hibana2077 hibana2077@gmaill.com
-LastEditTime: 2023-06-17 15:04:06
-FilePath: /NTTU-new-gen-judge-system/backend/apiServer.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse,HTMLResponse
 from pydantic import BaseModel
