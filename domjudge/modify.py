bc8c74e891175e1cf95b593b76e427ffd580c32b
hibana2077 <hibana2077@gmaill.com>
2024-01-05 14:30:55 +0800

    add modify python

---
+++ domjudge/modify.py
@@ -0,0 +1,9 @@
+'''
+Author: hibana2077 hibana2077@gmaill.com
+Date: 2024-01-05 14:30:08
+LastEditors: hibana2077 hibana2077@gmaill.com
+LastEditTime: 2024-01-05 14:30:17
+FilePath: /NTTU-judge-system/domjudge/modify.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import yaml
