c7f752539fcf06e641fe443273b3c7992d68a474
hibana2077 <hibana2077@gmail.com>
2024-01-06 00:10:05 +0800

    Update LastEditTime in setup.sh

--- domjudge/modify.py
+++ domjudge/modify.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2024-01-05 14:30:08
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-01-06 00:08:07
+LastEditTime: 2024-01-06 00:10:02
 FilePath: /NTTU-judge-system/domjudge/modify.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -92,5 +92,6 @@ if __name__ == '__main__':
         # save yaml
         write_yaml('./changed.yaml', data)
     else:
+        print(command, object_name)
         print("command error")
         sys.exit(0)
