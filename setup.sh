21fd93dea2a53a945c245ab2f8bf6f5929597495
hibana2077 <hibana2077@gmail.com>
2024-01-06 00:20:23 +0800

    Update api_secret extraction logic in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
  # @Author: hibana2077 hibana2077@gmail.com
  # @Date: 2023-11-09 10:52:11
  # @LastEditors: hibana2077 hibana2077@gmail.com
- # @LastEditTime: 2024-01-06 00:17:45
+ # @LastEditTime: 2024-01-06 00:20:19
  # @FilePath: \work_2023_fall\stop_all.sh
  # @Description: This script should be run in sudo mode. 
  # It includes error handling and user prompts for critical actions with color-coded messages.
@@ -120,7 +120,7 @@ echo -e "${YELLOW}Get the api secret...${NC}"
 domserver_id="domserver_${object_name}"
 docker exec -it ${domserver_id} cat /opt/domjudge/domserver/etc/restapi.secret > ./domjudge/api_secret.txt
 api_secret_content=$(cat ./domjudge/api_secret.txt)
-api_secret=$(echo "$api_secret_content" | awk '{print $4}')
+api_secret=$(echo "$api_secret_content" | grep "default" | awk '{print $NF}')
 echo -e "${GREEN}Successfully get the api secret.${NC}"
 echo -e "${YELLOW}The api secret is:${NC} ${GREEN}${api_secret}${NC}"
 
