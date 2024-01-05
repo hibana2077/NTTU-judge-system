9ccb3000126644d4dbfc5ee106dc88db6742eeb8
hibana2077 <hibana2077@gmail.com>
2024-01-06 00:04:55 +0800

    Fix email typo and update LastEditTime

--- domjudge/modify.py
+++ domjudge/modify.py
@@ -1,8 +1,8 @@
 '''
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2024-01-05 14:30:08
-LastEditors: hibana2077 hibana2077@gmaill.com
-LastEditTime: 2024-01-05 19:03:18
+LastEditors: hibana2077 hibana2077@gmail.com
+LastEditTime: 2024-01-06 00:04:36
 FilePath: /NTTU-judge-system/domjudge/modify.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -57,7 +57,6 @@ if __name__ == '__main__':
         command, object_name = input_list
     elif len(input_list) == 4:
         command, num, object_name, api_secret = input_list
-    print(command, object_name)
     if command == "startup":
         # read yaml
         data = read_yaml('./temp.yaml')
