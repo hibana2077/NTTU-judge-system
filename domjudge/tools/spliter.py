fd6cf6a1a1eec0d544f73fadbaa7bd40e80f312b
hibana2077 <hibana2077@gmail.com>
2024-04-16 23:14:47 +0800

    add a tool for get api_key

---
+++ domjudge/tools/spliter.py
@@ -0,0 +1,14 @@
+'''
+Author: hibana2077 hibana2077@gmail.com
+Date: 2024-04-16 23:10:56
+LastEditors: hibana2077 hibana2077@gmail.com
+LastEditTime: 2024-04-16 23:14:17
+FilePath: \simple-oj\domjudge\tools\spliter.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+data:list[str] = list()
+while True:
+    try:
+        data.append(input())
+    except EOFError:
+        print(data[-1].split()[-1])
