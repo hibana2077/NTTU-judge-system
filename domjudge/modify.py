2aa268ade0d6fecbf704c032d99e1974448bd7de
hibana2077 <hibana2077@gmail.com>
2024-04-17 00:28:52 +0800

    Update docker-compose.yml to include user=root for domserver and judgehost containers

--- domjudge/modify.py
+++ domjudge/modify.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2024-01-05 14:30:08
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-04-17 00:25:59
+LastEditTime: 2024-04-17 00:28:46
 FilePath: /NTTU-judge-system/domjudge/modify.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -27,6 +27,7 @@ def judge_host_template(num:int,object_name:str,api_secret:str,origin_dict:dict)
             f"judgehost_{i+1}_{object_name}": {
                 "container_name": f"judgehost_{i+1}_{object_name}",
                 "image": "domjudge/judgehost:latest",
+                "user": "root",
                 "privileged": True,
                 "hostname": f"judgedaemon_{i+1}",
                 "volumes": [
@@ -79,6 +80,7 @@ if __name__ == '__main__':
         data['services'][f'domserver_{object_name}']['container_name'] = f"domserver_{object_name}"
         data['services'][f'domserver_{object_name}']['links'] = [f"mariadb_{object_name}:mariadb"]
         data['services'][f'domserver_{object_name}']['privileged'] = True
+        data['services'][f'domserver_{object_name}']['user'] = "root"
 
         # save yaml
         write_yaml('./changed.yaml', data)
