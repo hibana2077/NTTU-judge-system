518dc1ab6221fff3adc03b7cffb0693a17778bd9
hibana2077 <hibana2077@gmail.com>
2024-04-17 00:17:30 +0800

    Update docker-compose.yml to use specific versions of domjudge/domserver and domjudge/judgehost

--- domjudge/modify.py
+++ domjudge/modify.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2024-01-05 14:30:08
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-01-06 01:09:50
+LastEditTime: 2024-04-17 00:16:58
 FilePath: /NTTU-judge-system/domjudge/modify.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -26,7 +26,7 @@ def judge_host_template(num:int,object_name:str,api_secret:str,origin_dict:dict)
         temp_dict = {
             f"judgehost_{i+1}_{object_name}": {
                 "container_name": f"judgehost_{i+1}_{object_name}",
-                "image": "domjudge/judgehost:latest",
+                "image": "domjudge/judgehost:2.8.0",
                 "privileged": True,
                 "hostname": f"judgedaemon_{i+1}",
                 "volumes": [
