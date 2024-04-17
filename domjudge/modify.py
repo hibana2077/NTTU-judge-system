42d8d1997c269a3fb5a77f4379857c02e9d41e83
hibana2077 <hibana2077@gmaill.com>
2024-04-17 10:17:39 +0800

    Update cgroup permissions for sys/fs/cgroup directories

--- domjudge/modify.py
+++ domjudge/modify.py
@@ -1,8 +1,8 @@
 '''
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2024-01-05 14:30:08
-LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-04-17 00:35:22
+LastEditors: hibana2077 hibana2077@gmaill.com
+LastEditTime: 2024-04-17 10:17:18
 FilePath: /NTTU-judge-system/domjudge/modify.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -30,7 +30,7 @@ def judge_host_template(num:int,object_name:str,api_secret:str,origin_dict:dict)
                 "privileged": True,
                 "hostname": f"judgedaemon_{i+1}",
                 "volumes": [
-                    "/sys/fs/cgroup:/sys/fs/cgroup:ro",
+                    "/sys/fs/cgroup:/sys/fs/cgroup:rw",
                 ],
                 "environment": [
                     "CONTAINER_TIMEZONE=Asia/Taipei",
